f5a39368de27b3f3d3aa68a072fcdcf54557c30b
redMedicaOftalmologica <githubred@gmail.com>
2016-08-02 23:59:12 -0500

    vista login autentificacion login-css-js

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+function onSignIn(googleUser) {
+     // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    
+    var gmail = profile.getEmail();
+    var id = profile.getId();
+    /*
+    document.write("ID: " + profile.getId()); // Don't send this directly to your server!
+    document.write('Full Name: ' + profile.getName());
+    document.write('Given Name: ' + profile.getGivenName());
+    document.write('F+amily Name: ' + profile.getFamilyName());
+    document.write("Image URL: " + profile.getImageUrl());
+    document.write("Email: " + profile.getEmail()); 
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    document.write("ID Token: " + id_token);
+    
+    var sistema = "http://localhost:8000/main?var1="+profile.getName();
+    location.href = sistema;
+    */
+    var inputGmail = document.getElementById('usuario');
+    var inputId = document.getElementById('contra');
+    
+    inputGmail.value = gmail;
+    inputId.value = id;
+    
+    document.getElementById('ingresa').click();
+}
--- resources/views/Autentificacion/principal.blade.php
+++ resources/views/Autentificacion/principal.blade.php
@@ -1,28 +1,62 @@
-@extends('plantillas.isede')
-@section('content')
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--links css y js-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!--links css propio-->
+        <link rel="stylesheet" href="css/login.css">
+        
+        <!--Google Sing-in-->
+        <meta name="google-signin-scope" content="profile email">
+        <meta name="google-signin-client_id" content="764218325264-h4c92vhnpj24719d226qdkkilbfvomtm.apps.googleusercontent.com">
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <h1>INICIAR SESION</h1>
+            </div>
+        </header>
+        <div class="container">
+            <div class="g-signin2" type="submit" data-onsuccess="onSignIn"></div>
+            <div class="secure">
+                
+                {!!Form::open(['route'=>'autentificacion.store','method'=>'POST'])!!}
 
-{!!Form::open(['route'=>'autentificacion.store','method'=>'POST'])!!}
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email de Usuario</label>
 
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email de Usuario</label>
+                            {!!Form::text('usuario',null,['required'=>' ', 'autocomplete'=>'off', 'placeholder' =>'Ejemplo@ejemplo.com','class'=>'form-control','id'=>'usuario'])!!}
+                    </div>
 
-            {!!Form::text('usuario',null,['required'=>' ', 'autocomplete'=>'off', 'placeholder' =>'Ejemplo@ejemplo.com','class'=>'form-control','id'=>'usuario'])!!}
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Id</label>
+
+                            {!!Form::password('contra',['required'=>' ', 'autocomplete'=>'off', 'placeholder' =>'Id','class'=>'form-control','id'=>'contra'])!!}
+                    </div>
+                    <hr>
+                    <center>
+                        <button type="submit" id="ingresa">Ingresar</button>
+                        <a href="#"><button type="button">Cancelar</button></a>
+                    </center>
+                    
+                {!!Form::close()!!}
+            </div>
+            <center>
+                <a href="#"><button type="button">Cancelar</button></a>
+            </center>
+            
+
         </div>
-
-    <div class="form-group">
-        <label for="exampleInputPassword1">Contraseña</label>
-
-            {!!Form::password('contra',['required'=>' ', 'autocomplete'=>'off', 'placeholder' =>'Ingrese su contraseña','class'=>'form-control','id'=>'contra'])!!}
-        </div>
-
-    <center><a href=""><span></span>¿Olvidaste tu contraseña?</a></center>
-    <hr>
-    <center>
-    <button type="submit" >Ingresar</button>
-    <a href="#">Cancelar</a>
-</center>
-
-{!!Form::close()!!}
-
-
-@stop
+        <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+        <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+        <script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+        <!--bootstrap js-->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <!--js propio-->
+        <script type="text/javascript" src="js/login.js"></script>
+    </body>
+</html>
