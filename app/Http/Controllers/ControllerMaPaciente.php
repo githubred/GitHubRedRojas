2c20417046dfb5470e749750b7744278910ecced
xMithrandir <githubred@gmail.com>
2016-08-05 11:55:12 -0500

    Controlador Paciente

---
+++ app/Http/Controllers/ControllerMaPaciente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ProyectoRedRojas\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use ProyectoRedRojas\Http\Requests;
+use ProyectoRedRojas\Http\Controllers\Controller;
+
+class ControllerMaPaciente extends Controller
+{
+    //
+}
